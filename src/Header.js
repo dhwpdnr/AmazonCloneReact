28fcec899b071ea7c4059eb3bd46f98ade71987e
dhwpdnr <ink1237@gmail.com>
2023-03-11 16:54:22 +0900

    [feat] header publish
    add header.js, header.css

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Header.css'
+import SearchIcon from "@material-ui/icons/Search"
+import {ShoppingBasket} from "@material-ui/icons";
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header_logo" src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo_image"/>
+            <div className="header_search">
+                <input className="header_searchInput" type="text"/>
+                <SearchIcon className="header_searchIcon"/>
+            </div>
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="header_optionLineOne">안녕하세요</span>
+                    <span className="header_optionLineTwo">로그인하기</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">돌아가기</span>
+                    <span className="header_optionLineTwo">주문내역</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">ㅇㅇㅇㅇ</span>
+                    <span className="header_optionLineTwo">ㄷㄷㄷㄷ</span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasket/>
+                    <span className="header_optionLineTwoheader_basketCount">
+                        0
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC50wLvXMABe5t5mtXcyRJUP9I-0563Pbk",
+    authDomain: "clone-d0e79.firebaseapp.com",
+    projectId: "clone-d0e79",
+    storageBucket: "clone-d0e79.appspot.com",
+    messagingSenderId: "205963801557",
+    appId: "1:205963801557:web:2df0b1b08e5b8261c6c35c",
+    measurementId: "G-S1HR2BZZKR"
+};
