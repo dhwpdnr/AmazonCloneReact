07e47d048fc1482d99d36de70e5a8a04c7559fe7
dhwpdnr <ink1237@gmail.com>
2023-03-15 19:14:55 +0900

    [feat] product css

--- src/Home.js
+++ src/Home.js
@@ -10,10 +10,13 @@ function Home() {
                      src="https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg" alt=""/>
 
                 <div className="home_row">
-                    <Product/>
+                    <Product id="2323" title="제품 1 입니다" price={3000}
+                             image="https:/en.pimg.jp/024/292/158/1/24292158.jpg" rating={5}/>
                 </div>
 
                 <div className="home_row">
+                    <Product/>
+                    <Product/>
                     <Product/>
                 </div>
 
